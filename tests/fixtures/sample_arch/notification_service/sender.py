4335909d48bb9ffee983814e41428760ec1558f1
Vikram <vkrmsatsangi@gmail.com>
2026-05-26 14:51:09 +0530

    fix: format imports

--- tests/fixtures/sample_arch/notification_service/sender.py
+++ tests/fixtures/sample_arch/notification_service/sender.py
@@ -1,7 +1,8 @@
 # notification_service/sender.py
 # This is a boundary violation! models is not public api of payment_service
 
-from payment_service.models import PaymentRecord # noqa: F401
+from payment_service.models import PaymentRecord  # noqa: F401
+
 
 def send_notification():
     pass
